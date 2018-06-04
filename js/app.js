a7af3b3ecf74abd4fdcab1cd79956bf5db180c72
Michał Karbasz <michal.karbasz@gmail.com>
2018-06-04 03:20:53 +0200

    added sound files and images

--- js/app.js
+++ js/app.js
@@ -1,12 +1,26 @@
 $(function () {
 
     const birdName = $('.flex-container span');
-    const description = $('.flex-container span p');
-    description.hide();
+    const description = $('.flex-container p');
+    description.slideUp();
 
-    birdName.on ('click', () => {
-        this.find('p').slideDown();
+    birdName.on ('click', function() {
+       const $this = $(this);
+       $this.parent().parent().find('.description p').slideToggle();
     })
 
+// Add sounds on image click
+
+    const sojkaImg = $('.sojka');
+    const sojkaSnd = new Audio ('sounds/porg.mp3')
+
+    sojkaImg.on ('click', function() {
+        playSound(sojkaSnd);
+    });
+
+
+    function playSound(bird) {
+        bird.play();
+    }
 
 });
