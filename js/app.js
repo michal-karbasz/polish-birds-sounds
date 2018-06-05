a227617be1d56896a52a73426a8c79f0db03eae0
Michał Karbasz <michal.karbasz@gmail.com>
2018-06-05 22:09:52 +0200

    added transparent icon on mouse hover

--- js/app.js
+++ js/app.js
@@ -1,7 +1,14 @@
 $(function () {
 
+
+// Slide down/up on click
+    
     const birdName = $('.flex-container span');
     const description = $('.flex-container p');
+    const noteImage = $('.flex-container .col');
+    const birdImage = $('.flex-container .col img');
+
+
     description.slideUp();
 
     birdName.on ('click', function() {
@@ -9,15 +16,36 @@ $(function () {
        $this.parent().parent().find('.description p').slideToggle();
     })
 
+//Add sound icon on mouse enter remove on mouseleave
+
+    birdImage.on('mouseover', function() {
+        const $this = $(this);
+        const notesImg = $('<img>')
+        notesImg.attr('src', 'img/sing.jpg');
+        notesImg.addClass('notes');
+        notesImg.addClass($this.attr('class'))
+        $this.parent().append(notesImg);
+    
+    })
+
+
+    noteImage.on('mouseleave', '.notes', function() {
+        const $this = $(this);
+            $this.remove();
+    })
+
 // Add sounds on image click
 
     const sojkaImg = $('.sojka');
     const sojkaSnd = new Audio ('sounds/porg.mp3')
 
-    sojkaImg.on ('click', function() {
-        playSound(sojkaSnd);
-    });
 
+    noteImage.on('click', '.sojka', function() {
+        playSound(sojkaSnd);
+    })
+
+
+// function to run bird's voice - new Audio is the parameter
 
     function playSound(bird) {
         bird.play();
